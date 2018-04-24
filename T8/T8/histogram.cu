90555228cb2269fd8213dccd349b11d05d7dfe3e
Ronnypetson Souza da Silva <ra211848@parsusy.lab.ic.unicamp.br>
2018-04-24 18:06:49 -0300

    cu

--- T8/T8/histogram.cu
+++ T8/T8/histogram.cu
@@ -95,17 +95,18 @@ static PPMImage *readPPM(const char *filename) {
 
 __global__ void count_hist(PPMImage *image, float *h, float n){
 	// compute i, j, k, l, x
-	unsigned int index = DIM_BLOCO*DIM_BLOCO*(DIM_GRID*blockIdx.x+blockIdx.y+blockDim.y*threadIdx.x+threadIdx.y;
+	unsigned int n_ = image->y*image->x;
+	unsigned int index = DIM_BLOCO*DIM_BLOCO*(DIM_GRID*blockIdx.x+blockIdx.y)+blockDim.y*threadIdx.x+threadIdx.y;
 	unsigned int x = index/64;
-	unsigned int i = index%n;
+	unsigned int i = index%n_;
 	int j = x/16;
 	int k = (x-16*j)/4;
 	int l = (x-16*j-4*k);
-	if (index < 64*image->y*image->x
+	if (index < 64*n_
 	 && image->data[i].red == j
 	 && image->data[i].green == k
 	 && image->data[i].blue == l) {
-		atomicAdd(h[x],1/n);
+		atomicAdd(&h[x],1/n);
 	}
 }
 
