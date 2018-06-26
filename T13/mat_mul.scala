bb8073732f344df46d29f98ce88c224508c8fb43
Ronnypetson <rsronnypetson4@gmail.com>
2018-06-26 20:38:56 -0300

    cmd

--- T13/mat_mul.scala
+++ T13/mat_mul.scala
@@ -11,18 +11,13 @@ object Mat_mul {
     // create Spark context with Spark configuration
     val sc = new SparkContext(new SparkConf().setAppName("Multiplicação de Matrizes"))
 
-    //val rows =  sc.parallelize(Seq(
-    //  (0L, Array(2.0, 0.0, 0.0)),
-    //  (0L, Array(0.0, 2.0, 0.0)),
-    //  (0L, Array(0.0, 0.0, 2.0)))
-    //).map{case (i, xs) => IndexedRow(i, Vectors.dense(xs))}
-
     val r = scala.util.Random
-    val rows_ = for (i <- 0 to 499) yield (for (j <- 0 to 99) yield r.nextDouble).toArray
+    val dim = args(0).toInt
+    val rows_ = for (i <- 0 to dim-1) yield (for (j <- 0 to dim-1) yield r.nextDouble).toArray
     val rows = sc.parallelize(rows_.toSeq).map{case rw => IndexedRow(0,Vectors.dense(rw))}
 
     val indexedRowMatrix = new IndexedRowMatrix(rows)
-    val localMatrix = Matrices.dense(100, 50, (for (i <- 0 to 100*50-1) yield r.nextDouble).toArray)
+    val localMatrix = Matrices.dense(dim, dim, (for (i <- 0 to dim*dim-1) yield r.nextDouble).toArray)
     indexedRowMatrix.multiply(localMatrix).rows.collect.foreach(println)
   }
 }
