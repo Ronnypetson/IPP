182df9407b5f00a67f0cb2eeba6a8784fb28eb36
Ronnypetson <rsronnypetson4@gmail.com>
2018-06-28 17:47:07 -0300

    fix names

--- T13/mat_mul.scala
+++ T13/mat_mul.scala
@@ -11,31 +11,22 @@ object Mat_mul {
     // create Spark context with Spark configuration
     val sc = new SparkContext(new SparkConf().setAppName("Multiplicação de Matrizes"))
 
-    val r = new scala.util.Random(1) // seed=1 para comparar os resultados
-    val dim = args(0).toInt
+    val r = new scala.util.Random(1) // Mesmo valor de 'seed' para comparar os resultados local e do cluster
+    val dim = args(0).toInt // Dimensao das matrizes aleatorias (matrizes quadradas)
+    val block_dim = 500 // Dimensao dos blocos das matrizes
 
     val rows_ = for (i <- 0 to dim-1) yield (i,(for (j <- 0 to dim-1) yield r.nextDouble).toArray)
-    val rows = sc.parallelize(rows_.toSeq).map{case (i,rw) => IndexedRow(i,Vectors.dense(rw))} // 0 -> i
-
-    //val indexedRowMatrix = new IndexedRowMatrix(rows)
-    //val localMatrix = Matrices.dense(dim, dim, (for (i <- 0 to dim*dim-1) yield r.nextDouble).toArray)
-    //indexedRowMatrix.multiply(localMatrix).rows.collect.foreach(println)
+    val rows = sc.parallelize(rows_.toSeq).map{case (i,rw) => IndexedRow(i,Vectors.dense(rw))}
 
     val rows2_ = for (i <- 0 to dim-1) yield (i,(for (j <- 0 to dim-1) yield r.nextDouble).toArray)
     val rows2 = sc.parallelize(rows2_.toSeq).map{case (i,rw) => IndexedRow(i,Vectors.dense(rw))}
 
-    val indexedRowMatrix = new IndexedRowMatrix(rows).toBlockMatrix().cache() //
-    indexedRowMatrix.validate() //
+    val M1 = new IndexedRowMatrix(rows).toBlockMatrix(block_dim,block_dim).cache()
+    M1.validate()
 
-    val localMatrix = new IndexedRowMatrix(rows2).toBlockMatrix().cache() // Matrices.dense(dim, dim, (for (i <- 0 to dim*dim-1) yield r.nextD$
-    localMatrix.validate()
+    val M2 = new IndexedRowMatrix(rows2).toBlockMatrix(block_dim,block_dim).cache()
+    M2.validate()
 
-    //println(indexedRowMatrix.numRows())
-    //println(indexedRowMatrix.numCols())
-    //println()
-    //println(localMatrix.numRows())
-    //println(localMatrix.numCols())
-
-    indexedRowMatrix.multiply(localMatrix).blocks.collect.foreach(println)
+    M1.multiply(M2).blocks.collect.foreach(println)
   }
 }
